086d51e3f10ae829cb9485e51ef469fe152020c3
Mainfrezzer <35963833+Mainfrezzer@users.noreply.github.com>
2025-09-16 11:08:47 +0200

    skip traceroute if custom
    this sidesteps an issue observed on protonvpn where the endpoint cant reach itself by the looks of it. rather odd.

--- additions/healthcheck.sh
+++ additions/healthcheck.sh
@@ -5,18 +5,18 @@ fi
 
 FAILURE_COUNT=$(cat "$HEALTHCHECK_FILE")
 
+if [ ! -z ${HEALTH_URL_CHECK} ]; then
+HOST=$HEALTH_URL_CHECK
+else
 if [ -z ${DISABLE_TUNNEL_MODE} ]; then
 PEER=$(grep -i "^Endpoint" "/etc/wireguard/wg0.conf" | head -n1 | cut -d'=' -f2 | tr -d ' ')
 HOST=$(echo "$PEER" | rev | cut -d':' -f2- | rev)
 HOST=$(echo "$HOST" | sed 's/^\[//;s/\]$//')
-HOST=$(traceroute -n -i wg0 "$HOST" | awk 'NR==2 {print $2}')
+HOST=$(traceroute -m 1 -w 1 -n -i wg0 "$HOST" | awk 'NR==2 {print $2}')
 else
 PEER=$(grep -i "^AllowedIP" "/etc/wireguard/wg0.conf" | head -n1 | cut -d'=' -f2 | tr -d ' ' | awk -F',' '{print $1}' | awk -F'/' '{print $1}')
-HOST=$(traceroute -n -i wg0 "$PEER" | awk 'NR==2 {print $2}')
+HOST=$(traceroute -m 1 -w 1 -n -i wg0 "$PEER" | awk 'NR==2 {print $2}')
 fi
-
-if [ ! -z ${HEALTH_URL_CHECK} ]; then
-HOST=$HEALTH_URL_CHECK
 fi
 
 ping -I wg0 -c 1 -W 20 "$HOST" > /dev/null 2>&1
